1190f3362d4e838a23ba3995119f050d9be6dc42
Ece Günay <62220352+ece-gunay@users.noreply.github.com>
2023-11-17 14:13:06 +0300

    Add files via upload

---
+++ Iteration-1/src/Advisor.java
@@ -0,0 +1,20 @@
+package oop_proje;
+import java.util.ArrayList;
+import java.util.List;
+public class Advisor extends Lecturer {
+	private ArrayList<Student> students;
+
+	public Advisor(String firstName, String lastName, String email, List<Course> assignedCourses,
+			ArrayList<Student> students) {
+		super(firstName, lastName, assignedCourses);
+		this.students = new ArrayList<Student>();
+	}
+
+	public ArrayList<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Student student) {
+		this.students.add(student);
+	}
+}
